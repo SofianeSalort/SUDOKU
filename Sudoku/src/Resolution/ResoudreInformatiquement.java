85698d89a4ace614ce75fc054ecd2839a188c738
Benoît Schmitt <Benoit.schmitt@etudiant.mines-nantes.fr>
2016-06-09 11:41:25 +0200

    Commencement de la résolution

--- Sudoku/src/Resolution/ResoudreInformatiquement.java
+++ Sudoku/src/Resolution/ResoudreInformatiquement.java
@@ -1,11 +1,44 @@
 package Resolution;
 
 public class ResoudreInformatiquement {
+	private Sudoku susu;
 	
 	
+	// resolution
+	public void resolution(){
+		for (int i=0;i<9;i++){
+			for (int j=0;j<9;j++){
+				int t = this.getSusu().getLignes().get(i).getCases().get(j).getValeur();
+				if(t != 0 ){
+					if(this.getSusu().getLignes().get(i). )
+					this.getSusu().getLignes().get(i).getCases().get(j).setValeur(t+1);
+
+				}
+			}
+					
+		}
+	while (this.getSusu().getLignes().get(i).possible(t) || this.getSusu().getColonnes().get(j).g )
+}
 	
-	
-	
-	
+	public void augmenter(int i,int j){
+		int t =this.getSusu().getLignes().get(i).getCases().get(j).getValeur();
+		if (t<9){
+			this.getSusu().getLignes().get(i).getCases().get(j).setValeur(t+1);
+		}
+		else {
+			this.getSusu().getLignes().get(i).getCases().get(j).setValeur(0);
+		}
+	}
+
+
+	public Sudoku getSusu() {
+		return susu;
+	}
+
+
+	public void setSusu(Sudoku susu) {
+		this.susu = susu;
+	}
+
 	
 }
